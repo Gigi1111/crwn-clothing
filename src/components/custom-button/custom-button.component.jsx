9e22f7cca766514e7936b86904223e21fe64626a
Gigi1111 <airolg1111@gmail.com>
2019-09-01 04:22:30 +0800

    styled components

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import './custom-button.styles.scss';
 
+//styled components
+import { CustonButtonContainer } from './custom-buton.styles';
+
+const CustomButton = ({ children, ...props }) => <CustonButtonContainer {...props}>{children}</CustonButtonContainer>;
+/*
 const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps }) => (
 	<button
 		className={`${isGoogleSignIn ? 'google-sign-in' : ''} ${inverted ? 'inverted' : ''}  custom-button`}
@@ -8,5 +13,5 @@ const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps }) => 
 	>
 		{children}
 	</button>
-);
+);*/
 export default CustomButton;
